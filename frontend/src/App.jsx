879a6134c5345272657e52e9550358a77b99cb97
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-08 18:16:39 +0530

    Added the dashboard to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentAuth from './pages/StudentAuth';
+import StudentDashboard from './pages/StudentDashBoard';
 
 function App() { 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StudentAuth />} />
+        <Route path="/Studentdashboard" element={<StudentDashboard />} />
       </Routes>
     </Router>
   )
