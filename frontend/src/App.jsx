f8145b5c2bb6cd3285af80e285c9176d27bdb553
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-09 21:23:54 +0530

    Updated the App.jsx with CompanyAuth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentAuth from './pages/StudentAuth';
 import StudentDashboard from './pages/StudentDashBoard';
+import CompanyAuth from './pages/CompanyAuth';
 
 function App() { 
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StudentAuth />} />
         <Route path="/Studentdashboard" element={<StudentDashboard />} />
+        <Route path="/CompanyAuth" element={<CompanyAuth />} />
       </Routes>
     </Router>
   )
