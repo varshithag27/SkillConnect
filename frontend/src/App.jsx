1519e810185e0147d5f58263a0d1204e3a994614
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-11 22:10:17 +0530

    Added the CompanyDashBoard to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentAuth from './pages/StudentAuth';
 import StudentDashboard from './pages/StudentDashBoard';
 import CompanyAuth from './pages/CompanyAuth';
+import CompanyDashboard from './pages/CompanyDashBoard';
 
 function App() { 
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<StudentAuth />} />
         <Route path="/Studentdashboard" element={<StudentDashboard />} />
         <Route path="/CompanyAuth" element={<CompanyAuth />} />
+        <Route path="/Companydashboard" element={<CompanyDashboard />} />
       </Routes>
     </Router>
   )
